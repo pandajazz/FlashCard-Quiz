1700ddf18896ca7b4e55ee6d20262cf0a31f1897
pandajazz <mosca.claud@gmail.com>
2020-06-19 11:55:30 +0100

    added List to App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FlashCardList from './FlashCardList'
+
 
 function App() {
+
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FlashCardList flashcards={flashcards} />
   );
 }
 
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2 + 2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+    question: 'Question 2',
+    answer: 'Answer',
+    options: [
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  }
+]
 export default App;
