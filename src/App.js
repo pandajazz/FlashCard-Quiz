3464ade3cebe4556ebe4e0f2174b3874c485806f
pandajazz <mosca.claud@gmail.com>
2020-06-24 13:56:46 +0100

    added API request and decodestring Function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 import FlashCardList from './FlashCardList';
 
@@ -8,6 +9,35 @@ import './app.css'
 function App() {
 
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+  useEffect(() => {
+    axios
+    .get('https://opentdb.com/api.php?amount=10')
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [
+          ...questionItem.incorrect_answers.map( a => decodeString(a)), 
+          answer
+        ]
+        return {
+          id: `${index}-${Date.now()}`,
+          questions: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+      console.log(res.data)
+    })
+
+  }, [])
+
+  function decodeString(string) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = string
+    return textArea.value
+  }
+
   return (
     <FlashCardList flashcards={flashcards} />
   );
